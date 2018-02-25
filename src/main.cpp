0bd5bdd817e1dfc04b325e9dec8d6ef5d0fe5c17
FNicon <mybeddrom@gmail.com>
2018-02-24 18:15:54 -0800

    Add 8 gedung

--- src/main.cpp
+++ src/main.cpp
@@ -26,8 +26,17 @@ int main()
 	//cout << endl << "Press CTRL + C to quit." << endl;
 
 	// Create Polygon
-	Polygon polygon1(0x9c6fba, 0, 1, 0x9c6fba, 0, 3);
-	Polygon polygon2(0x83cb69, 0, 1, 0x83cb69, 0, 4);
+	Polygon cadl(0x9c6fba, 0, 1, 0x9c6fba, 0, 3);
+	Polygon pau(0x9c6fba, 0, 1, 0x9c6fba, 0, 3);
+	Polygon perpus(0x9c6fba, 0, 1, 0x9c6fba, 0, 3);
+	Polygon cas(0x9c6fba, 0, 1, 0x9c6fba, 0, 3);
+	Polygon labtekX(0x9c6fba, 0, 1, 0x9c6fba, 0, 3);
+	Polygon oktagon(0x9c6fba, 0, 1, 0x9c6fba, 0, 3);
+	Polygon comlabs(0x9c6fba, 0, 1, 0x9c6fba, 0, 3);
+	Polygon labtekI(0x9c6fba, 0, 1, 0x9c6fba, 0, 3);
+
+	//Polygon polygon1(0x9c6fba, 0, 1, 0x9c6fba, 0, 3);
+	//Polygon polygon2(0x83cb69, 0, 1, 0x83cb69, 0, 4);
 
 	// Create vector of Transformations for each Polygon
 	vector<ITransform*> v1;
@@ -37,7 +46,61 @@ int main()
 	vector<ITransform*> v3;
 
 	// Add Points to Polygon
-	polygon1.add(160,160);
+	cadl.add(100,20);
+	cadl.add(200,20);
+	cadl.add(200,120);
+	cadl.add(100,120);
+	cadl.setCenter();
+
+	pau.add(200,0);
+	pau.add(250,0);
+	pau.add(250,10);
+	pau.add(270,10);
+	pau.add(270,20);
+	pau.add(280,20);
+	pau.add(280,120);
+	pau.add(220,120);
+	pau.add(220,20);
+	pau.add(200,20);
+	pau.setCenter();
+
+	perpus.add(350,0);
+	perpus.add(450,0);
+	perpus.add (450,100);
+	perpus.add (350,100);
+	perpus.setCenter();
+
+	cas.add(470,0);
+	cas.add (570,0);
+	cas.add (570,100);
+	cas.add (470,100);
+	cas.setCenter();
+
+	labtekX.add(0,140);
+	labtekX.add (120,140);
+	labtekX.add (120,240);
+	labtekX.add (0,240);
+	labtekX.setCenter();
+
+	oktagon.add(180,140);
+	oktagon.add (280,140);
+	oktagon.add (280,240);
+	oktagon.add (180,240);
+	oktagon.setCenter();
+
+	comlabs.add(350,140);
+	comlabs.add (450,140);
+	comlabs.add (450,240);
+	comlabs.add (350,240);
+	comlabs.setCenter();
+
+	labtekI.add(470,140);
+	labtekI.add (570,140);
+	labtekI.add (570,340);
+	labtekI.add (470,340);
+	labtekI.setCenter();
+
+	/*polygon1.add(160,160);
 	polygon1.add(220,160);
 	polygon1.add(250,212);
 	polygon1.add(220,263);
@@ -51,7 +114,7 @@ int main()
 	polygon2.add(250,297);
 	polygon2.add(190,297);
 	polygon2.add(160,242);
-	polygon2.setCenter();
+	polygon2.setCenter();*/
 	
 	// Create Transformation object
 	Translate translate;
@@ -59,10 +122,9 @@ int main()
 	Rotate rotate1, rotate2;
 
 	// Add Transformation rules
-	translate.set(5, 5, 30);
-	translate.set(5, -5, 30);
-	translate.set(-5, 5, 30);
-	translate.set(-5, -5, 30);
+	translate.set(5, 0, 30);
+	translate.set(0, 5, 30);
+	translate.set(0, -5, 30);
 
 	rotate1.set(10.0, 0);
 	rotate2.set(12.0, 0);
@@ -74,8 +136,15 @@ int main()
 	v3.push_back(&translate);
 
 	// Add Polygon and its vector of Transformation to World
-	world.addPolygon(&polygon1, &v1);
-	world.addPolygon(&polygon2, &v2);
+	world.addPolygon(&cadl, NULL);
+	world.addPolygon(&pau, NULL);
+	world.addPolygon(&perpus, NULL);
+	world.addPolygon(&cas, NULL);
+	world.addPolygon(&labtekX, NULL);
+	world.addPolygon(&oktagon, NULL);
+	world.addPolygon(&comlabs, NULL);
+	world.addPolygon(&labtekI, NULL);
+	//world.addPolygon(&polygon2, &v2);
 
 	// Add viewport Transformation to World
 	world.addViewportTrans(&v3);
